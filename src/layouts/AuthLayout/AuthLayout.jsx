f43f53db60591fd7cc08838fe089220a459a7e21
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2026-06-27 18:28:36 +0300

    feat: integrate Tailwind CSS and refactor application structure
    - Added Tailwind CSS and Axios as dependencies in package.json.
    - Removed App.css styles and replaced them with Tailwind CSS classes in App.jsx.
    - Created new Footer and Navbar components for layout.
    - Implemented AuthLayout and UserLayout for routing.
    - Added Home, Profile, Login, and Register pages.
    - Updated vite.config.js to include Tailwind CSS plugin.

---
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export class AuthLayout   {
+  render() {
+    return (
+      <>
+      AuthLayout
+        <Outlet/>
+      </>
+    )
+  }
+}
+
---
+++ src/layouts/UserLayout/UserLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+export default class UserLayout  {
+  
+
+  render() {
+    return <>
+     UserLayout
+     <Outlet/>
+    </>
+  }
+}
