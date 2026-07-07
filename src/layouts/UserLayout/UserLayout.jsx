fa9796d790762f4dec847bea244ab3466f208461
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2026-07-07 16:31:24 +0300

    refactor: remove Footer component from UserLayout

--- src/layouts/UserLayout/UserLayout.jsx
+++ src/layouts/UserLayout/UserLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom"
 import Navbar from '../../components/layout/Navbar/Navbar'
-import Footer from '../../components/layout/Footer/Footer'
 
 
 export default function UserLayout() {
@@ -11,7 +10,7 @@ export default function UserLayout() {
 
       <Outlet/>
       </div>
-      <Footer/>
+      
     </>
   )
 }
